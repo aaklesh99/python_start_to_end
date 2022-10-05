04ca7b7cc881126a41afbd1bbaacba09d28bf96a
Aklesh kumar <90383682+aaklesh99@users.noreply.github.com>
2022-10-05 17:37:25 +0530

    dictonary complete

--- distonary.py
+++ distonary.py
@@ -1,11 +1,16 @@
-d1={}
-print(type(d1))
-d2={"Akki":"C++","Ashish":"Html","Nitish":"Python","Shyam":{"fs":"Java","ss":"C++","ts":"JS"}}
-print(d2)
-print(d2["Shyam"]["ss"])
-d2["Utkarsh"] ="DSA"
-print(d2)
-d2[420]="Sales"
-print(d2)
-del d2[420]
-print(d2)
+# d1={}
+# print(type(d1))
+# d2={"Akki":"C++","Ashish":"Html","Nitish":"Python","Shyam":{"fs":"Java","ss":"C++","ts":"JS"}}
+# print(d2)
+# print(d2["Shyam"]["ss"])
+# d2["Utkarsh"] ="DSA"
+# print(d2)
+# d2[420]="Sales"
+# print(d2)
+# del d2[420]
+# print(d2)
+d ={"Akki":21,
+    "Ashish":22,
+    "prince":20}
+name=input()
+print(d[name])
---
+++ if_elif.py
@@ -0,0 +1,20 @@
+# num1=10
+# num2=int(input())
+# if num1>num2:
+#     print("Greater")
+# elif num1==num2:
+#     print("Equla")
+# else:
+#     print("Lesser")
+#
+# num =[1,2,3,4,5,6]
+# if 1 in  num:
+#     print("yes")
+
+age= int(input())
+if age>18:
+    print("You are eligible")
+elif age==18:
+    print("Come for physical test")
+else:
+    print("No, you are not eligible")
