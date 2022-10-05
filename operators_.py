0e1f8704de0447100b4d048e9a7f54a427ab5f7e
Aklesh kumar <90383682+aaklesh99@users.noreply.github.com>
2022-10-05 21:22:25 +0530

    till shorthand complete

---
+++ operators_.py
@@ -0,0 +1,57 @@
+# Operators in python
+# Arthematic oerators
+# Assignment operator
+# Comparision operator
+# logical operator
+# Identity operator
+# Membership operator
+# Bitwise operator
+
+#Arthematic oerators
+print("Arthematic oerators")
+print("Value of 6 + 5 is : ", 5+6)
+print("Value of 6 - 5 is : ", 5-6)
+print("Value of 6 * 5 is : ", 5*6)
+print("Value of 6 / 5 is : ", 5/6)
+
+# Assignment operator
+
+print("Assignment operator")
+x=5
+print(x)
+# x+=5
+# x-=5
+# x*=5
+x/=5
+print(x)
+
+print("Comparision operator")
+i=8
+print(i==8)
+print(i>=8)
+print(i<=8)
+
+print("Logical operator")
+a=True
+b=False
+print(a and a)
+print(a or b)
+print(a is not b)
+print(a is  b)
+
+print("Membership operator")
+list={1,23,54,234,45,234}
+print(13 in list)
+print(23 in list)
+
+print("Bitwise operator")
+print(1 & 0)
+print(1 & 2)
+print(1 & 3)
+
+
+
+
+
+
+
