9308dec8f996c17f4d9d60f944dd6ec4b81e5d5d
Aklesh kumar <90383682+aaklesh99@users.noreply.github.com>
2022-10-05 19:50:57 +0530

    while loop complete

--- loop.py
+++ loop.py
@@ -1,8 +1,16 @@
+        # for loop
 # list1={"Akki","Aklesh","Ashish","Nitish"}
 # for item in list1:
 #     print(item)
+#
+# list2={"int","float","Akki",2,52,1324,35,54,2,56}
+# for item in list2:
+#     if str(item).isnumeric() and item>6:
+#         print(item)
 
-list2={"int","float","Akki",2,52,1324,35,54,2,56}
-for item in list2:
-    if str(item).isnumeric() and item>6:
-        print(item)
+        #while Loop
+i=0
+while(i<45):
+    print(i)
+    #i=i+1
+    i+=1
