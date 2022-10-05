7f4d19e355effc9a7dd4cd14d176535160e26184
Aklesh kumar <90383682+aaklesh99@users.noreply.github.com>
2022-10-05 20:33:02 +0530

    faulty calculator complete

---
+++ break_and_continue.py
@@ -0,0 +1,21 @@
+# i=0
+# while(True):
+#     if(i+1<5):
+#         i=i+1
+#         continue
+#     print(i+1,end=" ")
+#     if(i==44):
+#         break
+#     i = i + 1
+
+while(True):
+    num=int(input("Enter a number : "))
+    if(num>100):
+        print("You have entered more than 100")
+        break
+    else:
+        print("Try angain..")
+        continue
+
+
+
---
+++ faulty_Calculator.py
@@ -0,0 +1,26 @@
+num1 = int(input("Enter first no :"))
+num2= int(input("Enter second no :"))
+operation = input("Enter operation (+,-,*,/) : ")
+#for Addition
+if(num1==56 and num2==9 and operation== "+"):
+    print("Sum =77")
+elif(num1==56 and num2==9 and operation== "-"):
+    print("Sub =908")
+elif(num1==45 and num2==3 and operation== "*"):
+    print("Mul =555")
+elif(num1==56 and num2==6 and operation== "/"):
+    print("Div =4")
+elif operation=="+":
+    print(num1+num2)
+elif operation=="-":
+    print(num1-num2)
+elif operation == "*":
+    print(num1*num2)
+elif operation=="/":
+    print(num1/num2)
+else:
+    print("Bye")
+
+
+
+
