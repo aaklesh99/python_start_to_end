29cfa80533f7bab8d29c11ae4f17ea265a71ae32
Aklesh kumar <90383682+aaklesh99@users.noreply.github.com>
2022-10-07 15:01:42 +0530

    module

---
+++ using.py
@@ -0,0 +1,12 @@
+import random
+# random_number = random.randint(0,5)
+# # print(random_number)
+# ran=random.random() *100
+# print(ran)
+
+# lst=["Akki","Raj","Aklesh Kumar","Ashish"]
+# print(random.choice(lst))
+
+lst = ["cloth","Fruit","Grocery","Vagitable"]
+choice = random.gauss(lst)
+print(choice)
