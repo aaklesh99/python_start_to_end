e9ccf7bc67005ea925848cfc4dba93cf33cc166f
Aklesh kumar <90383682+aaklesh99@users.noreply.github.com>
2022-10-08 12:33:05 +0530

    import complete

---
+++ main.py
@@ -0,0 +1,4 @@
+import main2
+a=main2.sum(10,5)
+print(a)
+print(main2.show("Akki"))
---
+++ main2.py
@@ -0,0 +1,10 @@
+def sum(n1,n2):
+    return n1+n2
+
+def show(str):
+    print(f"This is {str}")
+
+print("And the name is ",__name__)
+if __name__ == '__main__':
+    print(sum(6,4))
+    print(show("Akki"))
