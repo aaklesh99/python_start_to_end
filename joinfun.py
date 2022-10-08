c6ca52d78061ecd5f18a40379af943b01c4166a1
Aklesh kumar <90383682+aaklesh99@users.noreply.github.com>
2022-10-08 12:40:14 +0530

    join complete

---
+++ joinfun.py
@@ -0,0 +1,7 @@
+lst=["Akki","Ram","Aklesh","Ashish","Anand","Mohan","Sohan"]
+# for  item in lst:
+#     print(f"{item} and ",end=" ")
+
+a=", ".join(lst)
+print(a)
+print("This is example of join functon.")
